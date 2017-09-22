fd17c26405c7a3451b3fd18bce382f57a82dac2b
Sebastian Kaim <sebastian.kaim@novell.com>
2017-09-22 15:08:49 +0200

    Added helper to look at contents of p.gz files

---
+++ dumphelper.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+# vim: ts=4 et sw=4 sts=4 :
+
+# dumphelper.py - this program prints the contents of a dumpfile via sscanner.helper
+# Copyright (C) 2017 SUSE LINUX GmbH
+#
+# Author: Sebastian Kaim
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA.
+
+from sscanner import helper
+import pprint
+import sys
+
+if len(sys.argv) < 2:
+    print('Usage: %s <dump.p.gz> [depth]' % sys.argv[0])
+    sys.exit(1)
+
+data = helper.readPickle(sys.argv[1])
+
+depth=2
+if len(sys.argv) >= 3:
+    depth = int(sys.argv[2])
+
+pprint.pprint(data, indent=2, depth=depth)
+
