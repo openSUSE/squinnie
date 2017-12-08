openSUSE/squinnie

commit b0d9d496b317918f8c3ba64172d97762fbc8339e

probe/viewer: support for older systems w/o CapAmb or Seccomp
On SLE-11 we get a bunch of KeyErrors. These are fixed with this commit.

sscanner/viewer.py
@@ -357,7 +357,7 @@ class Viewer(object):
 
         elif column == ProcColumns.features:
             features = []
-            if pid_data["Seccomp"]:
+            if "Seccomp" in pid_data:
                 features.append("seccomp")
             if pid_data.get("root", "/") != "/":
                 features.append("chroot")
@@ -373,9 +373,12 @@ class Viewer(object):
             boring_caps = [0, 274877906943]
             all_uids_are_root = all_uids_equal and pid_data["Uid"][0] == 0
             no_uids_are_root = pid_data["Uid"].count(0) == 0
-            capabilities = pid_data[column_label]
+            capabilities = pid_data.get(column_label, None)
 
-            if all_uids_are_root:
+            if capabilities == None:
+                # e.g. CapAmb on SLE-11
+                return ""
+            elif all_uids_are_root:
                 # is root anyways
                 result = ""
             elif not self.m_verbose and capabilities in boring_caps:

sscanner/probe.py
@@ -187,6 +187,10 @@ class Scanner(object):
                 status_pid = {}
 
                 for key in field_transforms.keys():
+                    val = fields.get(key, None)
+                    if val == None:
+                        # e.g. on SLES-11 there is no CapAmb
+                        continue
                     transform_fnct = field_transforms[key]
                     status_pid[key] = transform_fnct(fields[key])
 

commit ca3bbf5f969853942a0eea839d6ffcdf2053253b

viewer: fixed regression introduced by b0d9d496b317918f8c3ba64172d97762fbc8339e
The seccomp feature was shown as always present, due to invalid
evaluation logic. This is now fixed.

sscanner/viewer.py
@@ -357,7 +357,7 @@ class Viewer(object):
 
         elif column == ProcColumns.features:
             features = []
-            if "Seccomp" in pid_data:
+            if pid_data.get("Seccomp", False):
                 features.append("seccomp")
             if pid_data.get("root", "/") != "/":
                 features.append("chroot")